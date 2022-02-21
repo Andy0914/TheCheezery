9af70300dfaf254e27d2f87035dce078ee1cbdc0
Andy0914 <urias.14@hotmail.com>
2022-02-20 22:49:26 -0700

    listView The Cheezery

---
+++ app/src/main/java/urias/andres/thecheezery/Product.kt
@@ -0,0 +1,8 @@
+package urias.andres.thecheezery
+
+import java.io.FileDescriptor
+
+data class Product(var name: String,
+                    var image: Int,
+                    var descriptor: String,
+                    var price: Double)
